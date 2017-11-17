ea614d2a5be4cfa17456e718c80e2faf9fdbcfae
Erin Combs <ecombs@triu.com>
2017-11-17 14:51:35 -0500

    Fixed create2 indentation

--- create2.php
+++ create2.php
@@ -2,11 +2,6 @@
 session_start();
 ?>
 <!DOCTYPE HTML>
-<!--
-	Dimension by HTML5 UP
-	html5up.net | @ajlkn
-	Free for personal and commercial use under the CCA 3.0 license (html5up.net/license)
--->
 <html>
 	<head>
 		<title>Rummage</title>
@@ -17,122 +12,119 @@ session_start();
 		<noscript><link rel="stylesheet" href="assets/css/noscript.css" /></noscript>
 	</head>
 	<body>
-
 		<!-- Wrapper -->
 		<div id="wrapper">
-
-				<!-- Header -->
-				<header id="header">
-					<div class="logo">
-						<span class="icon fa-trash"></span>
-					</div>
-					<div class="content">
-						<div class="inner">
-							<h1>Rummage</h1>
-							<p>A site to peruse, create, and manage <br> rummage, yard, and garage sales.</p>
-						</div>
-					</div>
-					<nav>
-						<ul>
-							<li><a href="#profile">Profile</a></li>
-							<li><a href="#create">Create</a></li>
-							<li><a href="#explore">Explore</a></li>
-							<!--<li><a href="#elements">Elements</a></li>-->
-							<li><a href="#sales">Sales</a></li>
-							<li><a href="#logout">Logout</a></li>
-						</ul>
-					</nav>
-				</header>
-
-				<!-- Main -->
-				<div id="main">
-
-					<!-- Intro -->
-					<article id="create">
-						<h2 class="major">Create</h2>
-						<!--<span class="image main"><img src="images/pic01.jpg" alt="" /></span>-->
-						<form action="create.php" method="post">
-							
-							Street Address: <input type="text" name="street" maxlength="40"><br>
-							City: <input type="text" name"city" maxlength="20"><br>
-							State: <input type="text" name="state" maxlength="2"><br>
-							Zip: <input type="text" name="zip" maxlength="5"><br>
-							<div class="field half first">
-                                                               		<input type="radio" id="community" name="type" value="c">
-                                                                        <label for="community">Community Rummage Sale</label>
-                                                                </div>
-							<div class="field half first">
-                                                                        <input type="radio" id="single" name="type" value="s">
-                                                                        <label for="single">Single Family Rummage Sale</label>
-                                                                      </div><br>
-
-
-							Date/Time: <br> <input type="datetime-local" name="eventdate" style="font-color: black"><br><br>
-							<input type="submit">
-						</form>	
-					</article>
-
-					<!-- Profile -->
-					<article id="profile">
-						<h2 class="major">Profile</h2>
-						<!--<span class="image main"><img src="images/pic02.jpg" alt="" /></span>-->
-						Username: <?php echo("{$_SESSION['username']}"."<br>"); ?><br>
-						First Name: <?php echo("{$_SESSION['firstname']}"."<br>"); ?><br>
-						Last Name: <?php echo("{$_SESSION['lastname']}"."<br>"); ?><br>
-						Password: <?php echo("{$_SESSION['password']}"."<br>"); ?><br>
-						Email: <?php echo("{$_SESSION['email']}"."<br>"); ?>
-
-					</article>
-
-
-					<!-- Saless -->
-					<article id="sales">
-						<section>
-							<h3 class="major">My Rummage Sales</h2>
-							<div class="table-wrapper">
-								<table class="alt">
-									<thead>
-										<tr>
-											<th>Street</th>
-											<th>City</th>
-											<th>State</th>
-											<th>Zip</th>
-											<th>Type</th>
-											<th>Event Date</th>
-										</tr>
-									</thead>
-									<tbody>
-										<?php
-									 	foreach ($_SESSION['salearray'] as &$value){
-											echo '<tr><td>' . $value['street'] . "</td>";
-											echo '<td>' . $value['city'] . "</td>";
-											echo '<td>' . $value['state'] . "</td>";
-											echo '<td>' . $value['zip'] . "</td>";
-											echo '<td>' . $value['type'] . "</td>" ;
-								 			echo '<td>' . $value['eventdate'] . "</td></tr>";
-										}
-										?>
-									</tbody>
-								</table>
-							</div>
-						</section>
-					</article>
+			<!-- Header -->
+			<header id="header">
+				<div class="logo">
+					<span class="icon fa-trash"></span>
 				</div>
+				<div class="content">
+					<div class="inner">
+						<h1>Rummage</h1>
+						<p>A site to peruse, create, and manage <br> rummage, yard, and garage sales.</p>
+					</div>
+				</div>
+				<nav>
+					<ul>
+						<li><a href="#profile">Profile</a></li>
+						<li><a href="#create">Create</a></li>
+						<li><a href="#explore">Explore</a></li>
+						<!--<li><a href="#elements">Elements</a></li>-->
+						<li><a href="#sales">Sales</a></li>
+						<li><a href="logout.php">Logout</a></li>
+					</ul>
+				</nav>
+			</header>
 
-				<!-- Footer -->
-				<footer id="footer">
-					<p class="copyright">&copy; Rummage.</p>
-				</footer>
+			<!-- Main -->
+			<div id="main">
+
+				<!-- Intro -->
+				<article id="create">
+					<h2 class="major">Create</h2>
+					<!--<span class="image main"><img src="images/pic01.jpg" alt="" /></span>-->
+					<form action="create.php" method="post">
+						<label for="street">Street Address</label> 
+						<input type="text" name="street" maxlength="40"><br>
+						<label for="city">City</label>
+						<input type="text" name"city" maxlength="20"><br>
+						<label for="state">State</label>
+						<input type="text" name="state" maxlength="2"><br>
+						<label for="zip">Zip</label> 
+						<input type="text" name="zip" maxlength="5"><br>
+						<div class="field half first">
+							<input type="radio" id="community" name="type" value="c">
+							<label for="community">Community Rummage Sale</label>
+						</div>
+						<div class="field half first">
+							<input type="radio" id="single" name="type" value="s">
+							<label for="single">Single Family Rummage Sale</label>
+						</div><br>
+						<label for="eventdate">Date/Time</label> 
+						<br> <input type="datetime-local" name="eventdate" style="font-color: black"><br><br>
+						<input type="submit">
+					</form>	
+				</article>
+
+				<!-- Profile -->
+				<article id="profile">
+					<h2 class="major">Profile</h2>
+					<!--<span class="image main"><img src="images/pic02.jpg" alt="" /></span>-->
+					Username: <?php echo("{$_SESSION['username']}"."<br>"); ?><br>
+					First Name: <?php echo("{$_SESSION['firstname']}"."<br>"); ?><br>
+					Last Name: <?php echo("{$_SESSION['lastname']}"."<br>"); ?><br>
+					Email: <?php echo("{$_SESSION['email']}"."<br>"); ?>
+				</article>
+
+				<!-- Saless -->
+				<article id="sales">
+					<section>
+						<h3 class="major">My Rummage Sales</h2>
+						<div class="table-wrapper">
+							<table class="alt">
+								<thead>
+									<tr>
+										<th>Street</th>
+										<th>City</th>
+										<th>State</th>
+										<th>Zip</th>
+										<th>Type</th>
+										<th>Event Date</th>
+									</tr>
+								</thead>
+								<tbody>
+									<?php
+								 	foreach ($_SESSION['salearray'] as &$value){
+										echo '<tr><td>' . $value['street'] . "</td>";
+										echo '<td>' . $value['city'] . "</td>";
+										echo '<td>' . $value['state'] . "</td>";
+										echo '<td>' . $value['zip'] . "</td>";
+										echo '<td>' . $value['type'] . "</td>" ;
+							 			echo '<td>' . $value['eventdate'] . "</td></tr>";
+									}
+									?>
+								</tbody>
+							</table>
+						</div>
+					</section>
+				</article>
 			</div>
 
+			<!-- Footer -->
+			<footer id="footer">
+				<p class="copyright">&copy; Rummage.</p>
+			</footer>
+		</div>
+
 		<!-- BG -->
-			<div id="bg"></div>
+		<div id="bg"></div>
 
 		<!-- Scripts -->
-			<script src="assets/js/jquery.min.js"></script>
-			<script src="assets/js/skel.min.js"></script>
-			<script src="assets/js/util.js"></script>
-			<script src="assets/js/main.js"></script>
+		<script src="assets/js/jquery.min.js"></script>
+		<script src="assets/js/skel.min.js"></script>
+		<script src="assets/js/util.js"></script>
+		<script src="assets/js/main.js"></script>
 
 	</body>
 </html>
