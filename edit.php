bba8356709ab4bcb6380f56217879cb8f0443943
Erin Combs <ecombs@triu.com>
2017-11-17 18:29:54 -0500

    hh

--- edit.php
+++ edit.php
@@ -24,7 +24,7 @@ $sql = "UPDATE yardsale set street='$street', city='$city', state='$state', zip=
 
 if ($mysqli->query($sql) === TRUE){
 	echo "Yard sale updated.";
-	header('Location: showSale.php?id=' . $saleid . '\'');
+	header("Location: showSale.php?id='" . $saleid . "'");
 } 
 else {
 	echo "Something went wrong.";
