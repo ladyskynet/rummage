748440c3ee85b0d34ed68cc5462d5058867ccf41
Erin Combs <ecombs@triu.com>
2017-11-17 20:58:49 -0500

    hh

--- welcome.php
+++ welcome.php
@@ -55,11 +55,12 @@ session_start();
 						<label for="zip">Zip</label> 
 						<input type="text" name="zip" maxlength="5"><br>
 
-
-						<select>
+						<label for="tellem">Sale Type</label>
+						<select id="tellem">
 							<option id="community" name="type" value="c">Community Rummage Sale</option>
 							<option id="single" name="type" value="s">Single Family Rummage Sale</option>
 						</select>
+						<br><br>
 						<label for="eventdate">Date/Time</label> 
 						<br> <input type="datetime-local" name="eventdate" style="font-color: black"><br><br>
 						<ul class="actions">
