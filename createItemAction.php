9c3fa42016bfb5c65c3d4be34718e724c416b837
Erin Combs <ecombs@triu.com>
2017-11-18 19:39:15 -0500

    hh

--- createItemAction.php
+++ createItemAction.php
@@ -15,15 +15,11 @@ $saleid = $mysqli->real_escape_string($_REQUEST['id']);
 $name = $mysqli->real_escape_string($_REQUEST['name']);
 $description = $mysqli->real_escape_string($_REQUEST['description']);
 $price = $mysqli->real_escape_string($_REQUEST['price']);
-echo $saleid . "<br>";
-echo $name . "<br>";
-echo $description . "<br>";
-echo $price . "<br>";
 
 $sql = "INSERT INTO item (name, description, price, pid, sid) VALUES ('$name', '$description', '$price', '1', '$saleid')"; 
 
-/***if($mysqli->query($sql) === true){
-	$url = 'showSale.php?id=' . $saleid
+if($mysqli->query($sql) === true){
+	$url = 'showSale.php?id=' . $saleid;
 	header('Location:' . $url );
 	echo "Rummage sale item created.";
 } else {
