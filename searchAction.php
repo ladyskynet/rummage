5f7dcae4ae27281a790bc8bbef0735312eebd71c
Erin Combs <ecombs@triu.com>
2017-11-20 17:45:56 -0500

    hh

--- searchAction.php
+++ searchAction.php
@@ -24,23 +24,23 @@ $string4 = "";
 $string5 = "";
 
 if (isset($street)){
-	$string1 = "AND (street='$street') ";
+	$string1 = " AND (street='$street')";
 }
 if (isset($city)){
-	$string2 = "AND (city='$city') ";
+	$string2 = " AND (city='$city')";
 }
 if (isset($street)){
-	$string3 = "AND (state='$state') ";
+	$string3 = " AND (state='$state')";
 }
 if (isset($city)){
-	$string4 = "AND (zip='$zip') ";
+	$string4 = " AND (zip='$zip')";
 }
 if (isset($item)){
-	$string5 = "AND (item in (select * from item where name='$item')) ";
+	$string5 = " AND (item in (select * from item where name='$item'))";
 }
 
-$sql = "select * from yardsale where 1=1 " . $string1 . $string2 . $string3 . $string4 . $string5;
-
+$sql = "select * from yardsale where 1=1" . $string1 . $string2 . $string3 . $string4 . $string5;
+echo $sql;
 $result = $mysqli->query($sql);
 
 if ($result->num_rows > 0){
@@ -50,7 +50,7 @@ if ($result->num_rows > 0){
 	#header('Location: results.php');
 	echo "Rummage sale created.";
 } else {
-	header('Location: welcome.php#profile');
+	#header('Location: welcome.php#profile');
 	echo "Something went wrong. " . $mysqli->error;
 }
 $mysqli->close();
