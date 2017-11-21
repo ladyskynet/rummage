5df4ec75cecb48baef112602e2d1b9bd6f855273
Erin Combs <ecombs@triu.com>
2017-11-21 02:14:50 -0500

    hh

--- orderAction.php
+++ orderAction.php
@@ -23,10 +23,10 @@ if ($mysqli->query($sql) === true){
 	$sql2 = "select id from payment where cardnumber='$cardnumber'";
 	$result2 = $mysqli->query($sql2);
 	$row2 = $result2->fetch_array();
-	/**$orid = $row2['id'];
-
+	$orid = $row2['id'];
+	echo $orid;
 	/**foreach $_SESSION['orderArray'] as $value {
-		/**$pid = $value[5];
+		$pid = $value[5];
 		$sql3 = "INSERT into orderitem (orid, pid) values ('$orid', '$pid')";
 		if ($mysqli->query($sql3)===true){
 			$url = 'showOrder.php?id=' . $saleid;
@@ -34,8 +34,8 @@ if ($mysqli->query($sql) === true){
 			echo "Order placed.";
 		} else {
 			echo "Something went wrong. " . $mysqli->error;
-		}**/
-	}
+		}
+	}**/
 }
 
 $mysqli->close();
