3bdfdea436289b1896c9e8ec29033f01c994ef9e
Erin Combs <ecombs@triu.com>
2017-11-21 12:25:52 -0500

    hh

--- editItemAction.php
+++ editItemAction.php
@@ -16,8 +16,46 @@ $id = $mysqli->real_escape_string($_REQUEST['id']);
 $name = $mysqli->real_escape_string($_REQUEST['name']);
 $description = $mysqli->real_escape_string($_REQUEST['description']);
 $price = $mysqli->real_escape_string($_REQUEST['price']);
+$promoted = $mysqli->real_escape_string($_REQUEST['promoted']);
+$userid = $_SESSION['id'];
+
+if ($promoted == ""){
+	$promoted = 'n';
+}
+
+$pid = 1;
 
 $sql = "UPDATE item set name='$name', description='$description', price='$price' WHERE id='$id'"; 
+
+$sql2 = "SELECT sid from item where id='$id'";
+$result2 = $mysqli->query($sql2);
+$row2 = $result2->fetch_array();
+$saleid = $row2['sid'];
+
+if ($promoted == 'y'){
+	if (isset($_SESSION['orderArray'])){
+		$orderDetailArray = array();
+		$orderDetailArray[0] = $saleid;
+		$orderDetailArray[1] = $userid;
+		$orderDetailArray[2] = $name;
+		$orderDetailArray[3] = $description;
+		$orderDetailArray[4] = $price;
+		$orderDetailArray[5] = $pid;
+		$num = count($_SESSION['orderArray']);
+		$_SESSION['orderArray'][$num] = $orderDetailArray;
+	} else {
+		$orderDetailArray = array();
+		$orderDetailArray[0] = $saleid;
+		$orderDetailArray[1] = $_SESSION['id'];
+		$orderDetailArray[2] = $name;
+		$orderDetailArray[3] = $description;
+		$orderDetailArray[4] = $price;
+		$orderDetailArray[5] = $pid;
+		$orderArray = array();
+		$orderArray[0] = $orderDetailArray;
+		$_SESSION['orderArray'] = $orderArray;
+	}
+} 
 
 if ($mysqli->query($sql) === TRUE){
 	echo "Item updated.";
