78f35bf1c1863281de4c813a1521fd53b7dfa4d7
Erin Combs <ecombs@triu.com>
2017-11-20 22:09:59 -0500

    hh

---
+++ cart.php
@@ -0,0 +1,100 @@
+<?php
+session_start();
+if (isset($_SESSION['id'])){
+	echo "";
+} else {
+	header('Location: index.html');
+}
+?>
+<!DOCTYPE HTML>
+<html>
+	<head>
+		<title>Rummage</title>
+		<meta charset="utf-8" />
+		<meta name="viewport" content="width=device-width, initial-scale=1, user-scalable=no" />
+		<link rel="stylesheet" href="assets/css/main.css" />
+		<!--[if lte IE 9]><link rel="stylesheet" href="assets/css/ie9.css" /><![endif]-->
+		<noscript><link rel="stylesheet" href="assets/css/noscript.css" /></noscript>
+  		<link rel="shortcut icon" type="image/x-icon" href="favicon.ico" />
+	</head>
+	<body>
+		<!-- Wrapper -->
+		<div id="wrapper">
+			<!-- Header -->
+			<header id="header">
+				<div class="logo">
+					<span class="icon fa-trash"></span>
+				</div>
+				<h2>My Cart</h2>
+				<div class="content">
+					<br>
+						<?php
+						$mysqli = new mysqli("localhost", "root", "password", "yardsale");
+
+						if ($mysqli === false){
+							die("ERROR: Could not connect. " . $mysqli->connect_error);
+						}
+
+						echo '<div class="table-wrapper">
+								<table class="alt">
+									<thead>
+										<tr>
+											<th>Sale ID</th>
+											<th>User ID</th>
+											<th>Name</th>
+											<th>Description</th>
+											<th>Set Price</th>
+											<th>Listing Price</th>
+										</tr>
+									</thead>
+									<tbody>';
+							foreach ($_SESSION['orderArray'] as $value) {
+								
+								$sql = "SELECT amount FROM price where id='$value[5]'"
+								$result = $mysqli->query($sql);
+								$row = $result->fetch_array();
+								$listingPrice = $row['amount'];
+					 			echo '<tr><td>' . $value[0] . "</td>";
+					 			echo '<tr><td>' . $value[1] . "</td>";
+					 			echo '<tr><td>' . $value[2] . "</td>";
+					 			echo '<tr><td>' . $value[3] . "</td>";
+					 			echo '<tr><td>' . number_format(round($value[4],2),2) . "</td>";
+					 			echo '<tr><td>' . $listingPrice . "</td></tr>";
+							}
+							echo '		</tbody>
+									</table>
+								</div>';
+						} else {
+							echo "<p>No items in your cart yet.</p>";
+						}	
+						?>
+				</div>
+				<nav>
+					<ul>
+						<li><a href="welcome.php#profile">Profile</a></li>
+						<li><a href="welcome.php#create">Create</a></li>
+						<li><a href="search.php">Search</a></li>
+						<!--<li><a href="#elements">Elements</a></li>-->
+						<li><a href="sales.php">Sales</a></li>
+						<li><a href="logoutAction.php">Logout</a></li>
+					</ul>
+				</nav>
+			</header>
+
+			<!-- Footer -->
+			<footer id="footer">
+				<p class="copyright">&copy; Rummage.</p>
+			</footer>
+		</div>
+
+		<!-- BG -->
+		<div id="bg"></div>
+
+		<!-- Scripts -->
+		<script src="assets/js/jquery.min.js"></script>
+		<script src="assets/js/skel.min.js"></script>
+		<script src="assets/js/util.js"></script>
+		<script src="assets/js/main.js"></script>
+
+	</body>
+</html>
--- createItemAction.php
+++ createItemAction.php
@@ -15,17 +15,41 @@ $saleid = $mysqli->real_escape_string($_REQUEST['id']);
 $name = $mysqli->real_escape_string($_REQUEST['name']);
 $description = $mysqli->real_escape_string($_REQUEST['description']);
 $price = $mysqli->real_escape_string($_REQUEST['price']);
+$promoted = $mysqli->real_escape_string($_REQUEST['promoted']);
 
-$sql = "INSERT INTO item (name, description, price, pid, sid, promoted) VALUES ('$name', '$description', '$price', '1', '$saleid', 'n')"; 
-echo $sql;
-if($mysqli->query($sql) === true){
+if ($promoted == 'y'){
+	if (isset($_SESSION['orderArray'])){
+		$orderDetailArray = array();
+		$orderDetailArray.array_push($saleid);
+		$orderDetailArray.array_push($_SESSION['id']);
+		$orderDetailArray.array_push($name);
+		$orderDetailArray.array_push($description);
+		$orderDetailArray.array_push($price);
+		$orderDetailArray.array_push(1);
+		$_SESSION['orderArray'].array_push($orderDetailArray);
+	} else {
+		$orderDetailArray = array();
+		$orderDetailArray.array_push($saleid);
+		$orderDetailArray.array_push($_SESSION['id']);
+		$orderDetailArray.array_push($name);
+		$orderDetailArray.array_push($description);
+		$orderDetailArray.array_push($price);
+		$orderDetailArray.array_push(1);
+		$orderArray = array();
+		$orderArray.array_push($orderDetailArray);
+		$_SESSION['orderArray'] = $orderArray;
+	}
+} 
+
+$sql2 = "INSERT INTO item (name, description, price, pid, sid, promoted) VALUES ('$name', '$description', '$price', '1', '$saleid', 'n')"; 
+if($mysqli->query($sql2) === true){
 	$url = 'showSale.php?id=' . $saleid;
-	#header('Location:' . $url );
+	header('Location:' . $url );
 	echo "Rummage sale item created.";
 } else {
-	#header('Location: welcome.php#profile');
 	echo "Something went wrong. " . $mysqli->error;
 }
+
 $mysqli->close();
 
 ?>
