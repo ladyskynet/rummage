cc6ace218446b299841a380da392a94b2317c3bf
Erin Combs <ecombs@triu.com>
2017-11-20 22:33:47 -0500

    hh

--- createItem.php
+++ createItem.php
@@ -26,60 +26,60 @@ if (isset($_SESSION['id'])){
 					<span class="icon fa-trash"></span>
 				</div>
 				<h2>Add Item</h2>
-				<div class="main">
-					<article>
-						<section>
-							<br>
-							<?php
-							$servername = "localhost";
-							$username = "root";
-							$password = "password";
-							$dbname = "yardsale";
-
-							$mysqli = new mysqli($servername, $username, $password, $dbname);
-
-							if ($mysqli === false){
-								die("Connection failed: " . $mysqli->connect_error);
-							}
-							$id = $mysqli->real_escape_string($_REQUEST['id']);
-							
-							echo '<form action="createItemAction.php" method="post" id="createitem">';
-							echo '<input type="hidden" name="id" value="' . $id .'"/>';
-							
-							echo '<label for="name">Name</label>';
-							echo'<input type="text" name="name" maxlength="40"/><br>';
-									
-							echo '<label for="description">Description</label>
-								<textarea rows="4" cols="50" name="description" form="createitem"></textarea><br>';
-								
-							echo '<label for="price">Price</label>';
-							echo '<input type="text" name="price"/><br>';
-							echo '<div class="field half first">
-									<input type="checkbox" id="promoted" name="promoted" value="y">
-									<label for="promoted">Promoted</label>
-								</div>'
-						
-							echo '<ul class="actions">';
-								echo '<li><input type="submit" class="button special" /></li>';
-								echo '<li><input type="reset" value="Reset" /></li>';
-							echo'</ul>
-							</form>';
-								$mysqli->close();
-							?>
-						</section>
-					</article>
-				</div>
-				<nav>
-					<ul>
-						<li><a href="welcome.php#profile">Profile</a></li>
-						<li><a href="wlecome.php#create">Create</a></li>
-						<li><a href="search.php">Search</a></li>
-						<!--<li><a href="#elements">Elements</a></li>-->
-						<li><a href="sales.php">Sales</a></li>
-						<li><a href="logoutAction.php">Logout</a></li>
-					</ul>
-				</nav>
 			</header>
+			<div class="main">
+				<article>
+					<section>
+						<br>
+						<?php
+						$servername = "localhost";
+						$username = "root";
+						$password = "password";
+						$dbname = "yardsale";
+
+						$mysqli = new mysqli($servername, $username, $password, $dbname);
+
+						if ($mysqli === false){
+							die("Connection failed: " . $mysqli->connect_error);
+						}
+						$id = $mysqli->real_escape_string($_REQUEST['id']);
+						
+						echo '<form action="createItemAction.php" method="post" id="createitem">';
+						echo '<input type="hidden" name="id" value="' . $id .'"/>';
+						
+						echo '<label for="name">Name</label>';
+						echo'<input type="text" name="name" maxlength="40"/><br>';
+								
+						echo '<label for="description">Description</label>
+							<textarea rows="4" cols="50" name="description" form="createitem"></textarea><br>';
+							
+						echo '<label for="price">Price</label>';
+						echo '<input type="text" name="price"/><br>';
+						echo '<div class="field half first">
+								<input type="checkbox" id="promoted" name="promoted" value="y">
+								<label for="promoted">Promoted</label>
+							</div>'
+					
+						echo '<ul class="actions">';
+							echo '<li><input type="submit" class="button special" /></li>';
+							echo '<li><input type="reset" value="Reset" /></li>';
+						echo'</ul>
+						</form>';
+							$mysqli->close();
+						?>
+					</section>
+				</article>
+			</div>
+			<nav>
+				<ul>
+					<li><a href="welcome.php#profile">Profile</a></li>
+					<li><a href="wlecome.php#create">Create</a></li>
+					<li><a href="search.php">Search</a></li>
+					<!--<li><a href="#elements">Elements</a></li>-->
+					<li><a href="sales.php">Sales</a></li>
+					<li><a href="logoutAction.php">Logout</a></li>
+				</ul>
+			</nav>
 
 			<!-- Main -->
 			<div id="main">
