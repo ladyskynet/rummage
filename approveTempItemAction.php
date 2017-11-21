c7797bdcfb17cbedc9cb6755b0ec04c72ad90109
Erin Combs <ecombs@triu.com>
2017-11-21 16:49:13 -0500

    hh

--- approveTempItemAction.php
+++ approveTempItemAction.php
@@ -18,7 +18,13 @@ $sql = "UPDATE item set promoted='y', approved='y' WHERE id='$id'";
 
 if ($mysqli->query($sql) === TRUE){
 	echo "Item updated.";
-	header('Location: showItem.php?id=' . $id);
+	$sql2 = "DELETE FROM temp WHERE itemid='$id'"; 
+	if ($mysqli->query($sql2) === TRUE){
+		echo "Item updated.";
+		header('Location: showItem.php?id=' . $id);
+	} else {
+		echo "Something went wrong." . $mysqli->error;
+	}
 } 
 else {
 	echo "Something went wrong." . $mysqli->error;
--- approveTempSaleAction.php
+++ approveTempSaleAction.php
@@ -17,9 +17,15 @@ $saleid = $mysqli->real_escape_string($_REQUEST['saleid']);
 $sql = "UPDATE yardsale set promoted='y', approved='y' WHERE id='$saleid'"; 
 
 if ($mysqli->query($sql) === TRUE){
-	echo "Yard sale updated.";
-	header('Location: showSale.php?id=' . $saleid);
-} 
+	echo "Sale updated.";
+	$sql2 = "DELETE FROM temp WHERE sid='$saleid'"; 
+	if ($mysqli->query($sql2) === TRUE){
+		echo "Sale updated.";
+		header('Location: showSale.php?id=' . $id);
+	} else {
+		echo "Something went wrong." . $mysqli->error;
+	}
+}  
 else {
 	echo "Something went wrong." . $mysqli->error;
 	#header('Location: welcome.php#sales');
