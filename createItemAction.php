fa10d9548feb5c622baf46cab481b044877a7b53
Erin Combs <ecombs@triu.com>
2017-11-20 23:14:07 -0500

    hh

--- createItemAction.php
+++ createItemAction.php
@@ -47,15 +47,11 @@ if ($promoted == 'y'){
 		$_SESSION['orderArray'] = $orderArray;
 	}
 } 
-echo $orderArray;
-echo $orderDetailArray;
-foreach ($_SESSION['orderArray'] as $value) {
-	echo $value[0] . " " . $value[1];
-}
+
 $sql2 = "INSERT INTO item (name, description, price, pid, sid, promoted) VALUES ('$name', '$description', '$price', '1', '$saleid', 'n')"; 
 if($mysqli->query($sql2) === true){
 	$url = 'showSale.php?id=' . $saleid;
-	#header('Location:' . $url );
+	header('Location:' . $url );
 	echo "Rummage sale item created.";
 } else {
 	echo "Something went wrong. " . $mysqli->error;
