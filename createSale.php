270146355a44b6a31fb12dc0478b1b0a24076ead
Erin Combs <ecombs@triu.com>
2017-11-21 22:50:16 -0500

    hh

--- createSale.php
+++ createSale.php
@@ -63,17 +63,19 @@ if (isset($_SESSION['id'])){
 
 							<label for="state">State</label>
 							<input type="text" name="state" maxlength="2" required/><br>
-							
+
 							<label for="zip">Zip</label> 
 							<input type="text" name="zip" maxlength="5" required/><br>
+							
 							<label for="type">Sale Type</label>
 							<select name="type">
 								<option value="c">Community Rummage Sale</option>
 								<option value="s">Single Family Rummage Sale</option>
-							</select>
-							<br>
+							</select><br>
+							
 							<input type="checkbox" id="promoted" name="promoted" value="y">
-							<label for="promoted">Promoted</label><br>
+							<label for="promoted">Promoted</label><br><br>
+
 							<label for="eventdate">Date/Time</label> 
 							<input type="datetime-local" name="eventdate" required/>
 							<ul class="actions">
