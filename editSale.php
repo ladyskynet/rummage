c4dcb24ae181804e7f2476c5eae04d685c39bca4
Erin Combs <ecombs@triu.com>
2017-11-24 16:26:50 -0500

    ff

--- editSale.php
+++ editSale.php
@@ -149,16 +149,20 @@ if (isset($_SESSION['id'])){
 					<ul>
 						<?php
 						if (isset($_SESSION['id'])){
-							echo '<li><a href="welcome.php#profile">Profile</a></li>
-							<li><a href="createSale.php">Create</a></li>
-							<li><a href="search.php">Search</a></li>
-							<li><a href="cart.php">My Cart</a></li>
-							<li><a href="sales.php">Sales</a></li>
-							<li><a href="logout.php">Logout</a></li>';
+							echo '<li><a href="welcome.php#profile">Profile</a></li>';
+							
 							if ($_SESSION['type'] == 'i'){
 								echo '<li><a href="prices.php">Prices</a></li>';
 								echo '<li><a href="approveTemp.php">Approve</a></li>';
+								echo '<li><a href="manageSales.php">All Sales</a></li>';
+							} else {
+								echo '<li><a href="createSale.php">Create</a></li>';
+								echo '<li><a href="cart.php">My Cart</a></li>';
+							    echo '<li><a href="sales.php">Sales</a></li>';
 							}
+							echo '<li><a href="search.php">Search</a></li>';
+							echo '<li><a href="logout.php">Logout</a></li>';
+
 						} else {
 							echo'<li><a href="index.html#join">Join</a></li>
 								<li><a href="index.html#login">Login</a></li>
