c22dfbfdd4b6cc38d5b859f45e3dfebb89c17143
Erin Combs <ecombs@triu.com>
2017-11-29 09:47:48 -0500

    hj

--- results.php
+++ results.php
@@ -57,8 +57,10 @@ session_start();
 						if ($item != ''){
 							$string5 = " AND (id in (select sid from item where name='$item'))";
 						}
+
+						$today = date("Y-m-d H:i:s");
 						
-						$sql = "select * from yardsale where 1=1" . $string1 . $string2 . $string3 . $string4 . $string5 . " order by approved desc";
+						$sql = "SELECT * from yardsale where 1=1" . $string1 . $string2 . $string3 . $string4 . $string5 . "WHERE eventdate >=" . $today "order by approved desc";
 					
 						$result = $mysqli->query($sql);
 
