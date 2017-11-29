5bd4484e47800f82b0762aa8ef54a597ab7f2b9b
Erin Combs <ecombs@triu.com>
2017-11-29 11:02:38 -0500

    hj

--- editSaleAction.php
+++ editSaleAction.php
@@ -29,7 +29,7 @@ if ($promoted == ""){
 
 $pid = 2;
 
-$sql = "UPDATE yardsale set street='$street', city='$city', state='$state', zip='$zip', type='$type', eventdate='$eventdate', enddate='$enddate' WHERE id='$saleid'";
+$sql = "UPDATE yardsale set street='$street', city='$city', state='$state', zip='$zip', type='$type', eventdate='$eventdate', enddate='$enddate', promoted='$promoted' WHERE id='$saleid'";
 
 if ($mysqli->query($sql) === TRUE){
 	echo "Yard sale updated.";
